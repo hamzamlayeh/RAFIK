a79522a74d3476cfbe5554ddc3b45046c052feff
yazidmissapui <yazid.missaoui@gmail.com>
2018-04-04 12:54:14 +0100

    Mail envoyé

---
+++ app/src/main/java/com/example/user/rafiki/Config.java
@@ -0,0 +1,6 @@
+package com.example.user.rafiki;
+
+public class Config {
+    public static final String EMAIL ="mobile.rafiki@gmail.com";
+    public static final String PASSWORD ="Rafiki2018$";
+}
