74329ffc62b34236db2cc5d71fe60580a7db2bf1
yazidmissapui <yazid.missaoui@gmail.com>
2018-04-15 17:05:52 +0100

    animation

--- app/src/main/java/com/example/user/rafiki/E8.java
+++ app/src/main/java/com/example/user/rafiki/E8.java
@@ -7,17 +7,72 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
+
 
 public class E8 extends AppCompatActivity {
 
     Intent intent;
-
+    Animation animation,animation2;
+ImageView coeur;
+TextView bpm;
+    final Random rand = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_e8);
 
+
+        animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        animation2= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+        coeur=(ImageView)findViewById(R.id.Coeur);
+        bpm = (TextView)findViewById(R.id.BPM_D);
+        coeur.startAnimation(animation2);
+
+        animation2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                int x = rand.nextInt(120);
+                coeur.startAnimation(animation);
+                bpm.setText(x+"");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                coeur.startAnimation(animation2);
+                int x = rand.nextInt(120);
+                bpm.setText(x+"");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
     }
     public void E10(View view) {
