9702432abcf522cff0dcc413c973544921d2e7d9
hamza mlayeh <hamza@outlook.com>
2018-04-18 21:35:30 +0100

    location

--- app/src/main/java/com/example/user/rafiki/E7_1.java
+++ app/src/main/java/com/example/user/rafiki/E7_1.java
@@ -1,6 +1,7 @@
 package com.example.user.rafiki;
 
 import android.Manifest;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
@@ -20,8 +21,7 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class E7_1 extends AppCompatActivity {
 
-    private static final int  REQUEST_ENABLE_BT = 1;
-    final static int MY_PERMISSIONS_REQUEST= 2;
+    private static final int REQUEST_ENABLE_BT = 1;
     boolean testconnexion;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
@@ -29,7 +29,6 @@ public class E7_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_e7_1);
-        checkFineLocationPermission();
         if (!BLEManager.isBLEEnabled(getApplicationContext())) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
@@ -43,78 +42,21 @@ public class E7_1 extends AppCompatActivity {
                             public void run() {
                                 if (BLEManager.isDeviceConnected()) {
                                     BLEManager.discoverDeviceServices();
-                                    testconnexion=true;
-                                    Intent ite=new Intent(E7_1.this,E7_2.class);
+                                    testconnexion = true;
+                                    Intent ite = new Intent(E7_1.this, E7_2.class);
                                     ite.putExtra("connexion", testconnexion);
                                     startActivity(ite);
                                 }
                             }
                         }, 2000);
-                    }else {// false
-                        testconnexion=false;
-                        Intent ite=new Intent(E7_1.this,E7_2.class);
+                    } else {// false
+                        testconnexion = false;
+                        Intent ite = new Intent(E7_1.this, E7_2.class);
                         ite.putExtra("connexion", testconnexion);
                         startActivity(ite);
                     }
                 }
             }, 2000);
-        }
-    }
-
-
-    public void checkFineLocationPermission()
-    {
-        if (ContextCompat.checkSelfPermission(E7_1.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(E7_1.this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed; request the permission
-                ;
-                ActivityCompat.requestPermissions(E7_1.this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                                Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
         }
     }
 }
