c6c8170eca275751988bd04d56e5eee31a5cfd7a
khaled <khaledhizawi@gmail.com>
2018-07-04 14:00:49 +0100

    Antecedents

--- app/src/main/java/com/example/user/rafiki/Adapter/Antecedents_Adapter.java
+++ app/src/main/java/com/example/user/rafiki/Adapter/Antecedents_Adapter.java
@@ -2,16 +2,21 @@ package com.example.user.rafiki.Adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.user.rafiki.ItemData.Antecedents_ItemData;
 import com.example.user.rafiki.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.user.rafiki.Adapter.Antecedents_Adapter.ViewHolder.TEST_VIDE;
 
 /**
  * Created by user on 29/06/2018.
@@ -22,6 +27,7 @@ public class Antecedents_Adapter extends RecyclerView.Adapter<Antecedents_Adapte
 public static class ViewHolder extends RecyclerView.ViewHolder{
     EditText e1;
     EditText e2;
+    public static int TEST_VIDE=1;
     public ViewHolder(View itemView) {
         super(itemView);
 
@@ -49,15 +55,78 @@ public Antecedents_Adapter(Context context, ArrayList<Antecedents_ItemData> list
     }
 
     @Override
-    public void onBindViewHolder(Antecedents_Adapter.ViewHolder holder, int position) {
-
-
-
+    public void onBindViewHolder(final Antecedents_Adapter.ViewHolder holder, int position) {
         Antecedents_ItemData itemData = list.get(position);
 
-    holder.e1.setText(itemData.getEdite1());
-    holder.e2.setText(itemData.getEdite2());
+            holder.e1.setText(itemData.getEdite1());
+            holder.e2.setText(itemData.getEdite2());
 
+        //        if (!itemData.getEdite1().equals("")&& itemData.getEdite2().equals("")){
+
+//        holder.e1.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if (holder.e2.getText().length()==0){
+//                    TEST_VIDE=1;
+//                }else {
+//                    TEST_VIDE=0;
+//                }
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if (holder.e2.getText().length()==0){
+//                    TEST_VIDE=1;
+//                }else {
+//                    TEST_VIDE=0;
+//                }
+//
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                if (holder.e2.getText().length()==0){
+//                    TEST_VIDE=1;
+//                }else {
+//                    TEST_VIDE=0;
+//                }
+//            }
+//
+//        });
+//        holder.e1.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if (holder.e1.getText().length()==0){
+//                    TEST_VIDE=1;
+//                }else {
+//                    TEST_VIDE=0;
+//                }
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if (holder.e1.getText().length()==0){
+//                    TEST_VIDE=1;
+//                }else {
+//                    TEST_VIDE=0;
+//                }
+//
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                if (holder.e1.getText().length()==0){
+//                    TEST_VIDE=1;
+//                }else {
+//                    TEST_VIDE=0;
+//                }
+//            }
+//
+//        });
+
+//
 
 
     }
--- app/src/main/java/com/example/user/rafiki/AntecedentsActivity.java
+++ app/src/main/java/com/example/user/rafiki/AntecedentsActivity.java
@@ -1,11 +1,15 @@
 package com.example.user.rafiki;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.user.rafiki.Adapter.Antecedents_Adapter;
@@ -15,23 +19,38 @@ import com.example.user.rafiki.Helper.RecyclerViewTouchListener;
 
 import java.util.ArrayList;
 
+import static com.example.user.rafiki.Adapter.Antecedents_Adapter.ViewHolder.TEST_VIDE;
+
 public class AntecedentsActivity extends AppCompatActivity {
     RecyclerView r;
+    EditText date,acte;
     Antecedents_Adapter myAdapter;
     ArrayList<Antecedents_ItemData> list;
-    int i=0,j=5;
-
+    ImageView ajout;
+    int i=0;
+    String TestDate,TestActe;
+    LinearLayout conteneur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_antecedents);
+        date=findViewById(R.id.date);
+        acte=findViewById(R.id.acte);
+        ajout=findViewById(R.id.ajout);
+        conteneur=findViewById(R.id.conteneur);
 
-
-
-        r = (RecyclerView) findViewById(R.id.r);
         list = new ArrayList<>();
+        r = (RecyclerView) findViewById(R.id.r);
+        myAdapter = new Antecedents_Adapter(this, list);
         r.setLayoutManager(new LinearLayoutManager(this));
         r.setAdapter(myAdapter);
+        if(list.size()>=5) {
+            ajout.setVisibility(View.VISIBLE);
+
+        }
+
+
+
 
 
         r.addOnItemTouchListener(new RecyclerViewTouchListener(getApplicationContext(), r, new RecyclerViewClickListener() {
@@ -44,13 +63,13 @@ public class AntecedentsActivity extends AppCompatActivity {
 
             @Override
             public void onLongClick(View view, int position) {
-                Toast.makeText(getApplicationContext(),  position+ " is long pressed!", Toast.LENGTH_SHORT).show();
                 try {
-
 
                     list.remove(position);
                     myAdapter.notifyDataSetChanged();
-
+                    if(list.size()>=4) {
+                        ajout.setVisibility(View.VISIBLE);
+                    }
                 }catch (Exception e){
                     e.getMessage();
                 }
@@ -58,18 +77,62 @@ public class AntecedentsActivity extends AppCompatActivity {
         }));
     }
     public void retoure_fiche(View view) {
-        Intent ite=new Intent(this,Fiche_MedicaleActivity.class);
-        startActivity(ite);
-    }
+
+
+   }
 
     public void ajout_linge(View view) {
-        if (i<=5) {
 
-            list.add(new Antecedents_ItemData(""+i , ""+i ));
-            myAdapter = new Antecedents_Adapter(this, list);
-            r.setLayoutManager(new LinearLayoutManager(this));
-            r.setAdapter(myAdapter);
-            i++;
+
+        TestDate=date.getText().toString();
+        TestActe=acte.getText().toString();
+
+        if(valider()){
+            if(list.size()<=5) {
+                list.add(new Antecedents_ItemData(TestDate, TestActe));
+                myAdapter.notifyDataSetChanged();
+                date.setText("");
+                acte.setText("");
+                ajout.setVisibility(View.VISIBLE);
+                Toast.makeText(this, ""+list.size(), Toast.LENGTH_SHORT).show();
+                if(list.size()>=5){
+                    ajout.setVisibility(View.GONE);
+                    conteneur.setVisibility(View.GONE);
+
+                }
+            }
+
         }
+
+
+
+//        for (i=0;i<list.size();i++) {
+
+//
+//                    myAdapter.notifyDataSetChanged();
+//                    myAdapter = new Antecedents_Adapter(this, list);
+//                    r.setLayoutManager(new LinearLayoutManager(this));
+//                    r.setAdapter(myAdapter);
+//                    i++;
+           //     }
+
+
+
+
+    }
+
+    @TargetApi(16)
+    private boolean valider() {
+        boolean valide = true;
+        if (TestDate.isEmpty()) {
+            date.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (TestActe.isEmpty()) {
+            acte.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+    return valide;
     }
 }
+
