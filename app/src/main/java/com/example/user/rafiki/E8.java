c98c8ea23420e4ae9492f9da6fb01ee04e14281d
hamzamlayeh <hamzamlayeh@outlook.com>
2018-12-31 01:08:28 +0100

    e8

--- app/src/main/java/com/example/user/rafiki/E8.java
+++ app/src/main/java/com/example/user/rafiki/E8.java
@@ -1,16 +1,22 @@
 package com.example.user.rafiki;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
@@ -308,6 +314,56 @@ public class E8 extends AppCompatActivity {
             Toast.makeText(activity, "aler 3", Toast.LENGTH_SHORT).show();
         }
     }
+
+//    public void Notification(Context context) {
+//
+//
+//        String CHANNEL_ID = "my_channel_01";
+//        CharSequence name = "my_channel";
+//        String Description = "This is my channel";
+//
+//        int NOTIFICATION_ID = 1;
+//
+//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//
+//            int importance = NotificationManager.IMPORTANCE_HIGH;
+//            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+//            mChannel.setDescription(Description);
+//            mChannel.enableLights(true);
+//            mChannel.setLightColor(Color.RED);
+//            mChannel.enableVibration(true);
+//            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+//            mChannel.setShowBadge(true);
+//
+//            if (notificationManager != null) {
+//
+//                notificationManager.createNotificationChannel(mChannel);
+//            }
+//
+//        }
+//        @SuppressLint("ResourceAsColor")
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+//                .setContentTitle("TITLE")
+//                .setContentText("SUB-TITLE")
+//                .setStyle(new NotificationCompat.BigTextStyle().bigText("Notice that the NotificationCompat.Builder constructor requires that you provide a channel ID. This is required for compatibility with Android 8.0 (API level 26) and higher, but is ignored by older versions By default, the notification's text content is truncated to fit one line. If you want your notification to be longer, you can enable an expandable notification by adding a style template with setStyle(). For example, the following code creates a larger text area"))
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                .setContentIntent(resultPendingIntent)
+//                .setAutoCancel(true)
+//                .setColor((android.R.color.holo_red_dark))
+//                .addAction(R.drawable.ic_launcher_foreground, "Call", resultPendingIntent)
+//                .addAction(R.drawable.ic_launcher_foreground, "More", resultPendingIntent)
+//                .addAction(R.drawable.ic_launcher_foreground, "And more", resultPendingIntent);
+//
+//
+//        if (notificationManager != null) {
+//
+//            notificationManager.notify(NOTIFICATION_ID, builder.build());
+//        }
+//    }
 
     public void Setvaluesbatement(int x) {
 
